c04bfec697c3ab0660b84ba8d0537e0de956d926
Prajjaval Singh <prajjavalsingh@gmail.com>
2022-04-24 15:02:37 +0530

    removed some variables

--- Account.py
+++ Account.py
@@ -15,8 +15,6 @@ class Account:
         self._nonce = 0
         self._private_pem       =     None
         self._public_pem        =     None
-        self.__private_key      =     None
-        self.__public_key       =     None
         self.__generate_key_pair()
 
     @property
@@ -40,11 +38,11 @@ class Account:
     def __generate_key_pair(self):
         # Implement key pair generation logic
         # Convert them to pem format strings and store in the class attributes already defined
-        self.__private_key = rsa.generate_private_key(public_exponent=65537,key_size=2048)
-        self.__public_key = self.__private_key.public_key()
-        self._private_pem = self.__private_key.private_bytes(encoding=serialization.Encoding.PEM,
+        private_key = rsa.generate_private_key(public_exponent=65537,key_size=2048)
+        public_key = private_key.public_key()
+        self._private_pem = private_key.private_bytes(encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.PKCS8,encryption_algorithm=serialization.NoEncryption())
-        self._public_pem = self.__public_key.public_bytes(encoding=serialization.Encoding.PEM,
+        self._public_pem = public_key.public_bytes(encoding=serialization.Encoding.PEM,
                     format=serialization.PublicFormat.SubjectPublicKeyInfo)
 
     def create_transaction(self, receiver_id, value, tx_metadata=''):
@@ -52,9 +50,11 @@ class Account:
         transaction_message = {'sender': self._id, 'receiver': receiver_id, 'value': value, 'tx_metadata': tx_metadata, 'nonce': nonce}
         # Implement digital signature of the hash of the message
         msg = b'You cannot eat your cake and have it too'
-        signature = self.__private_key.sign(msg,padding.PSS(mgf=padding.MGF1(hashes.SHA256()),
+        private_key = serialization.load_pem_private_key(self._private_pem,password=None)
+        signature = private_key.sign(msg,padding.PSS(mgf=padding.MGF1(hashes.SHA256()),
                                                             salt_length=padding.PSS.MAX_LENGTH),hashes.SHA256())
         signature = base64.b64encode(signature).decode("utf-8")
+        print( self._id + "    " + signature)
         self._nonce = nonce
         return {'message': transaction_message, 'signature': signature}
         
